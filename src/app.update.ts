612ee1a245c45572e52bc0003525c461ecbd739e
abzal <abzal008@mail.ru>
2023-02-19 10:51:24 +0600

    ---

--- src/app.update.ts
+++ src/app.update.ts
@@ -48,12 +48,13 @@ export class AppUpdate {
         const allPosts = await this.userService.getAllPosts(allUsers[0].id);
         if (allPosts.length) {
           allPosts.forEach((post) => {
-            ctx.telegram.sendMessage(ctx.from.id.toString(), `Пост id:${post.text}`);
+            ctx.telegram.sendMessage(ctx.from.id.toString(), `Пост id:${post.id}`);
             ctx.telegram.sendMessage(ctx.from.id.toString(), `Пост текст: \n${post.text}`);
           });
         } else {
           return ctx.telegram.sendMessage(ctx.from.id.toString(), "Постар жоқ");
         }
+        return;
       }
       if (message === "1") {
         ctx.session.type = "create_post";
